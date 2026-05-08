71320cb66119e01a970c7015fdf91f623662d7c6
Alexandr Iarmoshenko <a2398253@gmail.com>
2026-05-08 11:19:39 +0500

    Make b2b work

--- services/b2b/config.py
+++ services/b2b/config.py
@@ -1,21 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
+
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    HOST_DB: str
-    PORT_DB: int
-    POSTGRES_DB: str
+	database_url: str
 
-    @property
-    def database_url(self) -> str:
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.HOST_DB}:{self.PORT_DB}/{self.POSTGRES_DB}"
+	model_config = SettingsConfigDict(
+		extra="ignore",
+	)
 
-    model_config = SettingsConfigDict(
-        env_file=".env", 
-        env_file_encoding='utf-8',
-        extra="ignore",
-    )
 
 settings = Settings()
