6a749261bb4515b8b0c96f32beed555724f4d81b
Alexandr Iarmoshenko <a2398253@gmail.com>
2026-05-19 08:18:02 +0500

    Soft remove facets API endpont

--- services/b2c/api/catalog.py
+++ services/b2c/api/catalog.py
@@ -14,7 +14,7 @@ from core import db
 router = fastapi.APIRouter(prefix="/api/v1/catalog")
 
 
-@router.get("/facets")
+# @router.get("/facets")
 async def get_facets(
 	request: Request,
 	db_session: Annotated[AsyncSession, fastapi.Depends(db.get_db)],
