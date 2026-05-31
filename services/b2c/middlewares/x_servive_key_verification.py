7f375891018f35725c3256ee64ebc9545adf990c
VeRavenLTP <>
2026-05-31 18:44:10 +0500

    WIP save

--- services/b2c/middlewares/x_servive_key_verification.py
+++ services/b2c/middlewares/x_servive_key_verification.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Optional
 
 from fastapi import Request
 from fastapi.responses import JSONResponse
@@ -7,7 +7,17 @@ from core.config import settings
 
 SERVICE_API_PATHS = ["/api/v1/b2b/events"]
 
-def
+
+def _validate_service_key(request: Request) -> Optional[JSONResponse]:
+	if request.headers.get("X-Serivce-key") != settings.X_Service_Key:
+		return JSONResponse(
+			status_code=401,
+			content={
+				"code": "Invalid_X_Service_key",
+				"message": "X-Service-key is invalid",
+			},
+		)
+
 
 async def service_key_verification(
 	request: Request, call_next: Callable
@@ -15,3 +25,6 @@ async def service_key_verification(
 	if request.url.path not in SERVICE_API_PATHS:
 		return await call_next(request)
 
+	_validate_service_key(request)
+
+	return await call_next(request)
